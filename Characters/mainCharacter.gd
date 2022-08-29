cfc2fa1c59167766885e50651300a9cf09e486d2
Felipe Escárate <felipeescaratefernandez@gmail.com>
2022-08-29 11:46:22 -0400

    adding touch area

--- Characters/mainCharacter.gd
+++ Characters/mainCharacter.gd
@@ -1,10 +1,8 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var anim_player = $AnimationPlayer
+onready var anim_tree = $AnimationTree
+onready var playback = anim_tree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,4 +12,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("move_left"):
+		playback.travel("")
 		print("left")
