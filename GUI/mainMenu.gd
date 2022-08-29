3deb277d58ac941beb0bbd4d8c4496a837a8e6dd
Felipe Escárate <80856926+Feliesc@users.noreply.github.com>
2022-08-28 23:40:00 -0400

    Main menu auto-resizing
    menu resizing depending on the screen size

--- GUI/mainMenu.gd
+++ GUI/mainMenu.gd
@@ -1,15 +1,29 @@
 extends MarginContainer
 
-onready var start = $VBoxContainer/Start
-onready var settings = $VBoxContainer/Settings
-onready var exit = $VBoxContainer/Exit
+onready var buttons = $VBoxContainer/Buttons
+onready var start = $VBoxContainer/Buttons/Start
+onready var settings = $VBoxContainer/Buttons/Settings
+onready var exit = $VBoxContainer/Buttons/Exit
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	exit.connect("pressed", self, "_on_Exit_pressed")
+	# We set the container margin based on the window settings
+	margin_right = ProjectSettings.get_setting("display/window/size/width")
+	margin_bottom = ProjectSettings.get_setting("display/window/size/height")
+	
+	# Change font size
+	var scaleFactor = margin_right/270
+	var font = theme.get("default_font")
+	font.size *= scaleFactor
+	font.outline_size *= scaleFactor
+	
+	# Tittle-Buttons separation
+	$VBoxContainer.add_constant_override("separation", scaleFactor*20)
+		
+	# Buttons connections
 	start.connect("pressed", self, "_on_Start_pressed")
-	pass # Replace with function body.
+	exit.connect("pressed", self, "_on_Exit_pressed")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,7 +32,6 @@ func _ready():
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://Scenes/main.tscn")
-	pass # Replace with function body.
 
 func _on_Exit_pressed():
 	get_tree().quit()
