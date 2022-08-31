b77d399fc282f7d952e9729c3e63dd40298b1e1e
Felipe Escárate <felipeescaratefernandez@gmail.com>
2022-08-31 18:42:54 -0400

    First song added
    also added clicker constructor and three methods

--- Controllers/Clicker.gd
+++ Controllers/Clicker.gd
@@ -1,8 +1,47 @@
 extends Node
 class_name Clicker
 
-# Receives an InputEventScreenTouch and does an action
-func action(event: InputEventScreenTouch):
-	var x = event.position.x
-	var y = event.position.y
-	print(x, " ", y)
+var time0 : float # initial time, in microseconds
+var deltaT : float # difference between the current time and time0, in seconds
+var musicPlayer : AudioStreamPlayer
+var rhythm : Array
+var index : int
+
+func _init(player : AudioStreamPlayer):
+	# Load rhythm file
+	var file = File.new()
+	file.open("res://Assets/Songs/KomikuBicycle.rhythm", File.READ)
+	var content = file.get_as_text()
+	file.close()
+	self.rhythm = content.split("\n")
+	for i in range(len(self.rhythm)):
+		self.rhythm[i] = float(self.rhythm[i])
+	
+	self.time0 = OS.get_ticks_usec()
+	self.musicPlayer = player
+	self.musicPlayer.play()
+
+# It's called every cycle in order to refresh the rhythm
+func refreshNotes(label: Label) -> void:
+	var timef = OS.get_ticks_usec()
+	self.deltaT = (timef - time0) / 1000000.0 # We transform it to seconds
+	var diff = self.rhythm[self.index] - deltaT
+	label.text = str(diff)
+	if diff < -0.1:
+		advanceNote()
+
+# Receives an InputEventScreenTouch and returns a boolean that is true if the player has reached the rhythm and false if not
+func hasRhythm(event: InputEventScreenTouch) -> bool:
+	if self.rhythm[self.index] - self.deltaT < 0.1:
+		advanceNote()
+		print("OK",self.index)
+		return true
+	return false
+
+# Updates the next note that has to be touched
+func advanceNote():
+	self.index += 1
+	# We repeat the song rhythm
+	if self.index == len(self.rhythm):
+		self.time0 = OS.get_ticks_usec()
+		self.index = 0
--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 onready var settings = $Settings
-onready var currentController = Clicker.new()
+onready var musicPlayer = $AudioStreamPlayer
+onready var currentController = Clicker.new(musicPlayer)
+onready var isButtonPressed : bool = false
+onready var flow = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,12 +13,21 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	currentController.refreshNotes($Label)
 
 func _input(event):
+	
 	if event is InputEventScreenTouch and event.is_pressed():
-		currentController.action(event)
+		# If there is a button being pressed, nothing happens
+		if isButtonPressed:
+			isButtonPressed = false
+			return
+		
+		var hasRhythm = currentController.hasRhythm(event)
+		
+		
 
 func _on_Settings_Pressed():
+	isButtonPressed = true
 	print("settings")
