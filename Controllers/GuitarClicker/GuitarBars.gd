f362cacb63d6bacf6b9198d9814b9076c8000a34
Felipe Escárate <felipeescaratefernandez@gmail.com>
2022-10-12 14:31:54 -0300

    refactoring clicker class
    now Normal clicker works with a invisible button

--- Controllers/GuitarClicker/GuitarBars.gd
+++ Controllers/GuitarClicker/GuitarBars.gd
@@ -1,6 +1,29 @@
 extends Node2D
 
+onready var bar0 = $Bar
+onready var bar1 = $Bar2
+onready var bar2 = $Bar3
+onready var bar3 = $Bar4
+onready var bars : Array = [bar0, bar1, bar2, bar3]
+
+var depth : int
+
+var guitarClicker
+var main
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.y += 700
+	self.depth = bar0.margin_bottom
+	for bar in self.bars:
+		bar.guitarClicker = self.guitarClicker
+		bar.main = self.main
+
+func setParams(clicker, aMain):
+	self.guitarClicker = clicker
+	self.main = aMain
+	
+
+func addMark(speed: float, noteIndex: int):
+	var randIndex : int = randi()%4
+	bars[randIndex].guitarMark(speed, noteIndex)
 
--- Controllers/NormalClicker/NormalClicker.gd
+++ Controllers/NormalClicker/NormalClicker.gd
@@ -1,21 +1,27 @@
 extends Clicker
 class_name NormalClicker
 
-var displayIndex : int
 var bar : Sprite
 var barSize : float
 const SPEED: float = 3000.0
-onready var displayTime: float = 0.0
 
-
-func _init(player).(player):
+func _init(player, main).(player, main):
 	self.rhythm = getRhythmListFromFile("res://Assets/Songs/KomikuBicycle.tres")
 	# Play the song
 	self.musicPlayer.play()
 	
+	# creating a button to click
+	var button = Button.new()
+	button.margin_right = GlobalVars.width
+	button.margin_bottom = GlobalVars.height
+	button.modulate = Color(0,0,0,0)
+	button.connect("pressed", self, "isPressed")
+	self.add_child(button)
+		
 	# Bar
 	var rhythmBar = preload("res://Controllers/NormalClicker/rhythmBar.tscn")
 	self.bar = rhythmBar.instance()
+	self.bar.position = Vector2(GlobalVars.width/2, GlobalVars.height/10)
 	self.add_child(self.bar)
 	self.barSize = self.bar.size/2
 
@@ -33,8 +39,17 @@ func refreshNotes(delta) -> void:
 	
 	# Advance to the next note if the player didn't touch the current one
 	var diff = self.rhythm[self.index] - self.noteTime
-	if diff < -0.09:
+	if diff < -GlobalVars.GOOD_HIT:
 		advanceNote()
+		
+			
+func isPressed():		
+	if self.rhythm[self.index] - self.noteTime < 0.09:
+		advanceNote()
+		self.main.makeProgress()
+		return true
+	return false
+
 
 # Updates the next note that has to be touched
 func advanceNote():
