e54671732d7e84378b30f03d7558e054f4ca225d
Felipe Escárate <felipeescaratefernandez@gmail.com>
2022-11-24 03:17:42 -0300

    explosion update
    - sfx has been added
    - texture change

--- effects/explosion.gd
+++ effects/explosion.gd
@@ -6,25 +6,34 @@ extends Node2D
 onready var words = $Label 	
 onready var tween = $Tween
 onready var sprite = $Sprite
-onready var enemiesExplosionColors = [Color(0.65, 0.396, 0.9215, 1), Color(0.9686, 0.6235, 0.149, 1)]
+onready var sfx_player = $AudioStreamPlayer
+onready var enemiesExplosionColors = [Color("#d28c0dcc"), Color("#d2e59906")]
+onready var enemiesTextColors = [Color("#b789e9"), Color("#e0bc41")]
 var enemyType : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# explosion sound
+	self.sfx_player.stream = load("res://Assets/SFX/explosion.mp3")
+	self.sfx_player.play()
+	
+	
 	# Setting pause mode
 	self.pause_mode = Node.PAUSE_MODE_PROCESS 
-	get_tree().paused = true
+	#get_tree().paused = true
 	
 	# Setting the enemy message to the label
 	words.text = GlobalVars.enemiesMsgs[self.enemyType]
 	
-	var color1 = self.enemiesExplosionColors[self.enemyType]
+	self.sprite.modulate = self.enemiesExplosionColors[self.enemyType]
+	
+	var color1 = self.enemiesTextColors[self.enemyType]
 	var color2 = color1
 	color2.a = 0
 	tween.interpolate_property(words, "modulate", color2, color1, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.interpolate_property(words, "modulate", color1, color2, 2, 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.interpolate_property(sprite, "scale", Vector2(3,3), Vector2(8,8), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.interpolate_property(sprite, "scale", Vector2(8,8), Vector2(3,3), 2, 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)	
+	tween.interpolate_property(sprite, "scale", Vector2(1,1), Vector2(3.5,3.5), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(sprite, "scale", Vector2(3.5,3.5), Vector2(1,1), 2, 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)	
 	tween.start()
 
 func setEnemyType(enemyType : int):
